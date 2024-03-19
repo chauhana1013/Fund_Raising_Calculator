1f43a38f3b82b00ab62c2c268232f2974c3f6d50
chauhana1013 <100536433+chauhana1013@users.noreply.github.com>
2024-03-20 12:29:24 +1300

    Completed Yes No Checker

--- 00_FRC_BASE.py
+++ 00_FRC_BASE.py
@@ -1,6 +1,6 @@
 # Functions Here...
 
-# Checks users enter an integer to a given question
+# Checks that input is either a float or an integer that is more than zero. Takes in custom errors
 def num_check(question, error, num_type): 
     while True:
 
@@ -15,5 +15,29 @@ def num_check(question, error, num_type):
         except ValueError: 
             print(error)
 
+# Checks that user has enteres yes / no to a question
+def yes_no(question):
+
+    to_check = ["yes", "no"]
+
+    while True:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+        print("Please enter either yes or no...\n")
+
 
 # Main Routine Here...
+want_help = yes_no("Do want to read the instructions? ")
+
+get_int = num_check("How many do you need? ", "Please enter an amount more than 0\n", int)
+get_cost = num_check("How much does it cost? $", "Please enter a number more than 0\n", float)
+
+print(f"You need: {get_int}")
+print(f"It costs: ${get_cost}")
+
